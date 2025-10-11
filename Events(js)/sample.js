f0eeef0e74d5d9a53df8589db7d9fd6597e35df1
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-11 12:36:33 +0530

    study events

---
+++ Events(js)/sample.js
@@ -0,0 +1,10 @@
+
+function clickblue(){
+   document.getElementById('clicker').style.backgroundColor = 'blue';
+   document.getElementById('clicker').style.borderRadius = '100px';
+}
+
+function clickred(){
+   document.getElementById('clicker').style.backgroundColor = 'red';
+   document.getElementById('clicker').style.borderRadius = '0px';
+}
