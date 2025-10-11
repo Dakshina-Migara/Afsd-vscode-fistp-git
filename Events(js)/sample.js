8f7f3b61d94d611d3441da94f04003a31697be4e
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-11 15:28:53 +0530

    key events

--- Events(js)/sample.js
+++ Events(js)/sample.js
@@ -1,30 +1,48 @@
 // button eka click karaddi
-function clickblue(){
+function clickblue() {
    document.getElementById('clicker').style.backgroundColor = 'blue';
    document.getElementById('clicker').style.borderRadius = '100px';
 }
 
-function clickred(){
+function clickred() {
    document.getElementById('clicker').style.backgroundColor = 'red';
    document.getElementById('clicker').style.borderRadius = '0px';
 }
 
 
 // mouse eka move karaddi
-function mouseMove(){
+function mouseMove() {
    document.getElementById('mouseEvent').style.backgroundColor = 'blue';
    document.getElementById('mouseEvent').style.borderRadius = '100px';
 }
 
-function mouseOut(){
+function mouseOut() {
    document.getElementById('mouseEvent').style.backgroundColor = 'red';
    document.getElementById('mouseEvent').style.borderRadius = '0px';
 }
 
 // chat poto
-function mouseOut1(){
+function mouseOut1() {
    document.getElementById('chatpoto').style.backgroundImage = "url(../assets/chat-c-1.png)";
 }
-function mousein1(){
+function mousein1() {
    document.getElementById('chatpoto').style.backgroundImage = "url(../assets/chat-c.png)";
+}
+
+// key event
+function keyEvent1() {
+   console.log('keydown')
+}
+
+function keyEvent2() {
+   console.log('keyPress')
+}
+
+function keyEvent3() {
+   console.log('keyup')
+}
+
+function keyEvent4() {
+   const key01 = document.getElementById('keyup1').value;
+   console.log(key01);
 }
