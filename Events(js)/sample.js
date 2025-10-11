9f8cfbecc85f5da5b40398a6d1862795dfda42ef
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-11 12:48:49 +0530

    mouse move event

--- Events(js)/sample.js
+++ Events(js)/sample.js
@@ -1,4 +1,4 @@
-
+// button eka click karaddi
 function clickblue(){
    document.getElementById('clicker').style.backgroundColor = 'blue';
    document.getElementById('clicker').style.borderRadius = '100px';
@@ -7,4 +7,16 @@ function clickblue(){
 function clickred(){
    document.getElementById('clicker').style.backgroundColor = 'red';
    document.getElementById('clicker').style.borderRadius = '0px';
+}
+
+
+// mouse eka move karaddi
+function mouseMove(){
+   document.getElementById('mouseEvent').style.backgroundColor = 'blue';
+   document.getElementById('mouseEvent').style.borderRadius = '100px';
+}
+
+function mouseOut(){
+   document.getElementById('mouseEvent').style.backgroundColor = 'red';
+   document.getElementById('mouseEvent').style.borderRadius = '0px';
 }
