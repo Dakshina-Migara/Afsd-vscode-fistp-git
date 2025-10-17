74da49512ade21404dba8f432b9649450b60364a
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-10-17 19:22:01 +0530

    comple the tasks

--- Events(js)/Assignment07-home.js
+++ Events(js)/Assignment07-home.js
@@ -1,5 +1,5 @@
 function displayName() {
     let userName = document.getElementById("userName").value;
     sessionStorage.setItem('userName', userName);
-    window.location.href = 'quiz.html';
+    window.location.href = '../Events(js)/Assignment07-quiz.html';
 }
